1562a94b0af6b96bd796fe09fe05e3acbb5c0982
Tapas-kp <palaitapaskumar8@gmail.com>
2024-08-04 02:21:41 +0530

    fo

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Navbar/>
         <Home/>
         <About/>
-        {/* <Skills/> */}
+        <Skills/>
         {/* <Projects/> */}
         {/* <Gitstatus/> */}
         {/* <Contact/> */}
