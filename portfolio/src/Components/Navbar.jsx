73c303c4825bffcb110d36899a05ab5b6c017be3
Tapas-kp <palaitapaskumar8@gmail.com>
2024-08-05 00:07:11 +0530

    mm

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ import { Themecontext } from '../Contextprovider/Themecontext'
 
 
 
+
 export const Navbar = () => {
   const{mode,setmode}=useContext(Themecontext)
 
@@ -15,7 +16,9 @@ export const Navbar = () => {
   }
   return (
     <>
+    
     <div className="navbarMain">
+      
     <ul className='navbar' >
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
